a1db1f5f2bcb5af70b71edac69edeb94a931c15c
alisaif2015 <alisaif2015@gmail.com>
2019-03-04 14:52:27 -0600

    Update newBlast.py

--- newBlast.py
+++ newBlast.py
@@ -7,7 +7,7 @@ def main ():
   start = time.time()   
   for numHits in ["100","250","500","1000","5000","10000","20000"]:
     relative_name = "1_" + sys.argv[1] +"_Ref_Seq.fasta"
-    absolute_name = "/home/" + sys.argv[2] + "/outputFiles" + relative_name
+    absolute_name = "/home/" + sys.argv[2] + "/outputFiles/" + relative_name
     fasta_string = open(absolute_name).read()
     result_handle = NCBIWWW.qblast("blastp", "nr", fasta_string, hitlist_size=int(numHits))
     blast_result = open("2_"+ sys.argv[1] +"_" + numHits + "_BLAST.xml", "w")
