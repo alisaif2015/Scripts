594415bb2331b4ab6ecd5ec61246d3ea6a5074dd
alisaif2015 <alisaif2015@gmail.com>
2019-03-04 14:50:25 -0600

    Update newBlast.py

--- newBlast.py
+++ newBlast.py
@@ -1,12 +1,14 @@
 def main ():
-  import time,sys
+  import time,sys,os
   from Bio import Entrez, SeqIO
   from Bio.Blast import NCBIWWW
 
   
   start = time.time()   
   for numHits in ["100","250","500","1000","5000","10000","20000"]:
-    fasta_string = open("1_" + sys.argv[1] +"_Ref_Seq.fasta").read()
+    relative_name = "1_" + sys.argv[1] +"_Ref_Seq.fasta"
+    absolute_name = "/home/" + sys.argv[2] + "/outputFiles" + relative_name
+    fasta_string = open(absolute_name).read()
     result_handle = NCBIWWW.qblast("blastp", "nr", fasta_string, hitlist_size=int(numHits))
     blast_result = open("2_"+ sys.argv[1] +"_" + numHits + "_BLAST.xml", "w")
     blast_result.write(result_handle.read())
