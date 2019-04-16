163c9c1ae51b09ec32b041b670188ab2a1e8ffb6
alisaif2015 <alisaif2015@gmail.com>
2019-04-16 17:07:23 -0500

    Update newTranspose.py

--- newTranspose.py
+++ newTranspose.py
@@ -1,6 +1,10 @@
 def main():
   import pandas as pd
   import sys
+  print("\n")
+  print("Transposing files...")
   for file in ["2_" + sys.argv[1] + "_100_BLAST.csv", "2_" + sys.argv[1] + "_250_BLAST.csv", "2_" + sys.argv[1] + "_500_BLAST.csv", "2_" + sys.argv[1] + "_1000_BLAST.csv", "2_" + sys.argv[1] + "_5000_BLAST.csv", "2_" + sys.argv[1] + "_10000_BLAST.csv", "2_" + sys.argv[1] + "_20000_BLAST.csv"]:
     pd.read_csv(file).T.to_csv(file,header=False)
+  print("\n")
+  print("Done transposing files...")
 main()
