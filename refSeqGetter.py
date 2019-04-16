1617ab6a2b37ac81e7221bdf78b2cdb123684112
alisaif2015 <alisaif2015@gmail.com>
2019-04-16 16:50:41 -0500

    Update refSeqGetter.py

--- refSeqGetter.py
+++ refSeqGetter.py
@@ -3,6 +3,7 @@ import sys
 Entrez.email = sys.argv[1]
 proteinName = sys.argv[2]
 accessionNum = sys.argv[3]
+print("Starting to fetch the Reference Sequence for the protein associated with the accession number:",accessionNum)
 download_handle = Entrez.efetch(db="protein", id=accessionNum, rettype="fasta", retmode="text")
 data = download_handle.read()
 download_handle.close()
@@ -11,3 +12,4 @@ download_handle.close()
 out_handle = open("1_"+ proteinName +"_Ref_Seq.fasta", "w")
 out_handle.write(data)
 out_handle.close()
+print("Done getting Reference Sequence!")
